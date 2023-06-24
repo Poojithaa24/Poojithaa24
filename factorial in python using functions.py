76abf4739fdd6df0d58fe30ddeb8cde84c278c3a
Poojithaa24 <137624595+Poojithaa24@users.noreply.github.com>
2023-06-24 20:41:36 +0530

    factorial in python using functions.py

--- factorial in python using functions.py
+++ factorial in python using functions.py
@@ -1,8 +1,8 @@
 #Factoial program using functions
 def factorial(n):
   fact = 1
-  for i in range (n,0,-1):
-    fact = fact * i
+  for i in range (n,0,-1):             #decrementing the value from n to 1
+    fact = fact * i                     #evaluating factorial
   print("Factoial of",n,'is :',fact)
 
 n = int(input("Enter n value : "))
